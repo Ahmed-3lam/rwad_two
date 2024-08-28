c3706f719b13fdb0775f9bdb2b7ec037ac6ad7af
Ahmed Allam <3lam.ahmedd@gmail.com>
2024-08-28 18:31:41 +0300

    Home(2)

--- lib/dummy_projects/ecommerce/main/main_screen.dart
+++ lib/dummy_projects/ecommerce/main/main_screen.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int selectedIndex = 0;
+  List<Widget> _screens = [
+    Container(
+      child: Center(
+        child: Text(
+          "Home",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text(
+          "Profile",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text(
+          "Setting",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    ),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
+      body: _screens[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        onTap: (index) {
+          selectedIndex = index;
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
+        ],
       ),
     );
   }
